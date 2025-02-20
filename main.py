ddf649fc85f77c536bcfaa3ead85ca4e381b3d30
aqwarius2003 <00victorr@gmail.com>
2025-02-20 14:43:22 +0400

    Divided functions into parts, simplified logging

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import logging
 from urllib.request import urlopen, Request
 from urllib.error import URLError
 
+
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
@@ -13,9 +14,7 @@ logging.basicConfig(
 
 def download_archive(url, save_path):
     """Загружает ZIP-архив по указанному URL и сохраняет его на диск."""
-    os.makedirs(os.path.dirname(save_path), exist_ok=True)
     req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-
     with urlopen(req, timeout=10) as response:
         if response.status != 200:
             raise URLError(f"HTTP Error {response.status}")
@@ -23,15 +22,12 @@ def download_archive(url, save_path):
         with open(save_path, 'wb') as f:
             f.write(response.read())
 
-    logging.info(f"Архив успешно загружен: {save_path}")
-
 
 def unzip_archive(zip_file_path, output_folder):
     """Распаковывает ZIP-архив в указанную папку."""
-    os.makedirs(output_folder, exist_ok=True)
     with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
         zip_ref.extractall(output_folder)
-    logging.info(f'Архив {zip_file_path} распакован в {output_folder}')
+
 
 def main():
     url_archive = 'https://dvmn.org/media/modules_dist/quiz-questions.zip'
@@ -40,18 +36,20 @@ def main():
     output_folder = os.path.join(current_directory, 'data', 'QA_FOLDER')
 
     try:
+        os.makedirs(os.path.dirname(zip_file_path), exist_ok=True)
+        os.makedirs(output_folder, exist_ok=True)
+
         logging.info("Начало загрузки архива...")
         download_archive(url_archive, zip_file_path)
 
         logging.info("Начало распаковки архива...")
         unzip_archive(zip_file_path, output_folder)
 
-        logging.info("Обработка завершена успешно")
-
     except Exception as e:
-        logging.error(f"Ошибка выполнения: {type(e).__name__} - {str(e)}")
+        logging.error(f"Ошибка при скачивании или распаковке архива: {type(e).__name__} - {str(e)}")
     finally:
-        logging.info("Работа программы завершена")
+        logging.info(f'Архив {zip_file_path} успешно распакован в {output_folder}')
+
 
 if __name__ == "__main__":
     main()
